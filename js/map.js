e699374d0d5a43b3af71bee6c5242b4f1d8aa737
Bruno De Bondt <mail@brunodbo.be>
2014-03-21 11:41:53 -0700

    Remove layer switcher

--- js/map.js
+++ js/map.js
@@ -2,29 +2,25 @@
 
 // Set up map.
 var mapId = 'brunodbo.gka6ciem';
-if ($(window).width() < 626) {
-  var map = L.mapbox.map('map', mapId).setView([54, -121], 6);
-} else {
-  var map = L.mapbox.map('map', mapId, {
-    layersControl: {
-      position: 'bottomleft'
-    },
-    legendControl: {
-      position: 'bottomright'
-    },
-    attributionControl: {
-      position: 'bottomright'
-    },
-    minZoom: 6,
-    maxZoom: 8,
-    maxBounds: L.latLngBounds(L.latLng(49.15296, -135.7470), L.latLng(59.9660, -99.3823)),
-    keyboard: false
-  }).setView([54, -116.5], 6);
-}
+var map = L.mapbox.map('map', mapId, {
+  layersControl: {
+    position: 'bottomleft'
+  },
+  legendControl: {
+    position: 'bottomright'
+  },
+  attributionControl: {
+    position: 'bottomright'
+  },
+  minZoom: 6,
+  maxZoom: 8,
+  maxBounds: L.latLngBounds(L.latLng(49.15296, -135.7470), L.latLng(59.9660, -99.3823)),
+  keyboard: false
+}).setView([54, -116.5], 6);
 
-var baseLayers;
-var overlays;
+// var baseLayers;
+// var overlays;
 
-var layerControl = L.control.layers(baseLayers, overlays);
-layerControl.setPosition('bottomleft');
-layerControl.addTo(map);
+// var layerControl = L.control.layers(baseLayers, overlays);
+// layerControl.setPosition('bottomleft');
+// layerControl.addTo(map);
