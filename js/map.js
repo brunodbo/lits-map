4a8700beaaa8a6e14cc73f6e4e548ee290477602
Bruno De Bondt <mail@brunodbo.be>
2014-04-28 15:58:27 -0700

    Adding new locations

--- js/map.js
+++ js/map.js
@@ -12,8 +12,14 @@ var map = L.mapbox.map('map', mapId, {
   attributionControl: {
     position: 'bottomright'
   },
-  minZoom: 6,
-  maxZoom: 8,
+  // minZoom: 6,
+  // maxZoom: 6,
   maxBounds: L.latLngBounds(L.latLng(49.15296, -135.7470), L.latLng(59.9660, -99.3823)),
-  keyboard: false
+  keyboard: false,
+  zoomControl: false
 }).setView([54, -123.5], 6);
+
+// Disable zoom.
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
